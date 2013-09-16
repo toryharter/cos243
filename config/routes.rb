0f41938e56b3a7634f5c38c3b8efb29dc74788a6
Ben Duggan <benaduggan@gmail.com>
2013-09-16 13:19:24 +0000

    Fixed some things... had issues with rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-GameContestServer::Application.routes.draw do
+GameContestServer::Application.routes.draw do  
   
   resources :users
+  
+  get 'signup', to: 'users#new', as: 'signup'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/users_pages_spec.rb
+++ spec/requests/users_pages_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe "UsersPages" do
   describe "Sign Up" do
     it "allows the user to fill in name and password" do
-      visit "/users/new"
-      fill_in 'username', with: 'user@example.com'
-      fill_in 'password', with: 'password'
-      fill_in 'confirmation', with: 'password'
+      visit signup_path
+      fill_in 'Username', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Confirmation', with: 'password'
       click_button 'Create new account'
     end
   end
